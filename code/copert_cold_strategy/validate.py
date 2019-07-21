fa70f185edb3a8738eba463daebd9a6e7fbea272
twollnik <tom.wollnik@student.hpi.de>
2019-07-21 14:47:35 +0200

    check if hot strategy path is valid in validation for copert cold strategy

--- code/copert_cold_strategy/validate.py
+++ code/copert_cold_strategy/validate.py
@@ -3,6 +3,7 @@ import logging
 import pandas as pd
 
 from code.copert_hot_strategy.validate import validate_copert_yeti_format_files, validate_copert_berlin_format_files
+from code.script_helpers.dynamic_import_from import dynamic_import_from
 from code.strategy_helpers.validation_helpers import check_separator_is_comma, check_column_names, \
     check_categories_are_correct
 
@@ -23,6 +24,8 @@ def validate_copert_cold_berlin_format_files(**kwargs):
     validate_cold_ef_table(cold_ef_file)
     validate_veh_mapping(veh_mapping_file)
 
+    check_if_hot_strategy_path_is_valid(**kwargs)
+
 
 def validate_copert_cold_yeti_format_files(**kwargs):
 
@@ -39,6 +42,8 @@ def validate_copert_cold_yeti_format_files(**kwargs):
 
     validate_cold_ef_table(cold_ef_file)
     validate_veh_mapping(veh_mapping_file)
+
+    check_if_hot_strategy_path_is_valid(**kwargs)
 
 
 def validate_cold_ef_table(filename):
@@ -57,3 +62,15 @@ def validate_veh_mapping(filename):
 
     check_separator_is_comma(filename)
     check_column_names(filename, data, ["VehCat", "Fuel", "VehSegment", "EuroStandard", "Technology", "VehName"])
+
+
+def check_if_hot_strategy_path_is_valid(**kwargs):
+
+    hot_strategy_path = kwargs.get("hot_strategy")
+
+    if hot_strategy_path is not None:
+        try:
+            dynamic_import_from(hot_strategy_path)
+        except:
+            logging.warning(f"Path to hot strategy seems to be invalid. Please check the parameter 'hot_strategy' "
+                            f"in the config file. You passed: {hot_strategy_path}")
