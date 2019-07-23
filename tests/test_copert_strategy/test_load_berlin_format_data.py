3dd5668b6927d58d7c54dfe715e5cfca67996543
twollnik <tom.wollnik@student.hpi.de>
2019-07-23 16:54:42 +0200

    improve test for load berlin format data function

--- tests/test_copert_strategy/test_load_berlin_format_data.py
+++ tests/test_copert_strategy/test_load_berlin_format_data.py
@@ -18,19 +18,19 @@ class TestLoadBerlinFormatDataForCopertStrategy(TestCase):
            return_value={"some": "return", "value": "for mocking"})
     def test_case_only_hot(self, mocked_copert_hot_load_function):
 
-        actual_return_value = load_copert_berlin_format_data(only_hot=True, test_arg1=1, test_arg2="abc")
+        actual_return_value = load_copert_berlin_format_data(only_hot=True, hot_test_arg1=1, cold_test_arg2="abc")
 
         self.assertEqual(actual_return_value, {"some": "return", "value": "for mocking"})
-        mocked_copert_hot_load_function.assert_called_once_with(only_hot=True, test_arg1=1, test_arg2="abc")
+        mocked_copert_hot_load_function.assert_called_once_with(only_hot=True, hot_test_arg1=1, cold_test_arg2="abc")
 
     @patch("code.copert_strategy.load_berlin_format_data.load_copert_cold_berlin_format_data",
            return_value={"some": "return", "value": "for mocking"})
     def test_case_hot_and_cold_using_copert_cold_strategy(self, mocked_copert_cold_load_function):
 
-        actual_return_value = load_copert_berlin_format_data(test_arg1=1, test_arg2="abc")
+        actual_return_value = load_copert_berlin_format_data(hot_test_arg1=1, cold_test_arg2="abc")
 
         self.assertEqual(actual_return_value, {"some": "return", "value": "for mocking"})
-        mocked_copert_cold_load_function.assert_called_once_with(test_arg1=1, test_arg2="abc")
+        mocked_copert_cold_load_function.assert_called_once_with(hot_test_arg1=1, cold_test_arg2="abc")
 
     @patch("code.copert_strategy.load_berlin_format_data.load_copert_hot_berlin_format_data",
            return_value={"some": "return", "value": "for mocking"})
